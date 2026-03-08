78383fa9ba130162ef634e7963ac1ddf2098e022
Pavan Kumar <pavankumar9014663184@gmail.com>
2026-03-08 13:47:13 +0530

    Refactor Home component to use Link for navigation; update NotFound message capitalization

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,29 +1,23 @@
-import {useHistory} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 import Header from '../Header'
 import './index.css'
 
-const Home = () => {
-  const history = useHistory()
-
-  const onClickFindJobs = () => {
-    history.push('/jobs')
-  }
-
-  return (
-    <div className="home-container">
-      <Header />
-      <div className="home-content">
-        <h1 className="home-heading">Find The Job That Fits Your Life</h1>
-        <p className="home-description">
-          Millions of people are searching for jobs, salary information, company
-          reviews. Find the job that fits your abilities and potential.
-        </p>
-        <button className="find-jobs-btn" onClick={onClickFindJobs}>
+const Home = () => (
+  <div className="home-container">
+    <Header />
+    <div className="home-content">
+      <h1 className="home-heading">Find The Job That Fits Your Life</h1>
+      <p className="home-description">
+        Millions of people are searching for jobs, salary information, company
+        reviews. Find the job that fits your abilities and potential.
+      </p>
+      <Link to="/jobs" className="find-jobs-link">
+        <button type="button" className="find-jobs-btn">
           Find Jobs
         </button>
-      </div>
+      </Link>
     </div>
-  )
-}
+  </div>
+)
 
 export default Home
